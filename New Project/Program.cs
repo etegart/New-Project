9e00c2415fd1aab75619c3dbea016b5483305d6d
etegart <etegart@gmail.com>
2019-02-03 20:30:00 -0600

    Sunday changes

---
+++ New Project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace New_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It's Sunday!");
+            Console.WriteLine("What's going on?");
+        }
+    }
+}
